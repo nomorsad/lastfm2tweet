1ad0a60cd4863fe1694699f707bbe19f9dccbe8a
nomorsad <nomorsad.poubelle@gmail.com>
2014-11-22 18:53:55 +0100

    let Pycharm reformat code

--- lastfmtweet/lastfmtweet.py
+++ lastfmtweet/lastfmtweet.py
@@ -7,7 +7,7 @@ lastfmtweet -- Publish Lastfm charts on Twitter
 
 @copyright:  2014 organization_name. All rights reserved.
 
-@license:    GPLv3 or
+@license:    GPLv3 or newer
 
 @contact:    nomorsad.poubelle@gmail.com
 @deffield    updated: Updated
@@ -15,17 +15,17 @@ lastfmtweet -- Publish Lastfm charts on Twitter
 
 import sys
 import os
-
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
-
 import urllib2
-from defusedxml import ElementTree
 import re
 import string
 from datetime import date
-import tweepy
 import json
+
+from defusedxml import ElementTree
+import tweepy
+
 
 __all__ = []
 __version__ = 0.1
@@ -36,39 +36,43 @@ DEBUG = 0
 TESTRUN = 0
 PROFILE = 0
 
+
 class CLIError(Exception):
     '''Generic exception to raise and log different fatal errors.'''
+
     def __init__(self, msg):
         super(CLIError).__init__(type(self))
         self.msg = "E: %s" % msg
+
     def __str__(self):
         return self.msg
+
     def __unicode__(self):
         return self.msg
 
+
 def blacklist(name):
     '''hardcoded list of excluded items'''
-    
-    #TODO: move list into config file
-    hardcoded_blacklist=[
-                         'comptines',
-                         ]
-    
-    regex = re.compile('%s'%string.join(hardcoded_blacklist,'|'),re.I)
-    return regex.search(name)
-            
-    
 
-def get_print_list(username,chart,period):
+    # TODO: move list into config file
+    hardcoded_blacklist = [
+        'comptines',
+    ]
+
+    regex = re.compile('%s' % string.join(hardcoded_blacklist, '|'), re.I)
+    return regex.search(name)
+
+
+def get_print_list(username, chart, period):
     '''return LastFM XML chart as a simple list'''
-    
-    url='http://ws.audioscrobbler.com/2.0/user/%s/top%ss.xml?period=%s' % (username,chart,period)
+
+    url = 'http://ws.audioscrobbler.com/2.0/user/%s/top%ss.xml?period=%s' % (username, chart, period)
     print url
     raw_xml = urllib2.urlopen(url)
 
-    print_list=[]
-    charts=ElementTree.fromstring(raw_xml.read())
-    
+    print_list = []
+    charts = ElementTree.fromstring(raw_xml.read())
+
     if chart == 'artist':
         for artist in charts.findall('artist'):
             print_list.append(artist.find('name').text)
@@ -82,17 +86,17 @@ def get_print_list(username,chart,period):
                 print_list.append("%s - %s" % (artist.find('name').text, track.find('name').text))
     else:
         raise CLIError(Exception("unknown type %s" % chart))
-        
+
     return print_list
 
 
 def summarize(print_list, prefix='', limit=140):
     '''prepare message to be published'''
-    text=prefix
+    text = prefix
     for add in print_list:
-        
+
         if not blacklist(add):
-            new_len = len(text) + len(add) +2
+            new_len = len(text) + len(add) + 2
             if new_len > limit:
                 break
             else:
@@ -102,24 +106,24 @@ def summarize(print_list, prefix='', limit=140):
 
 def publish_twitter(text):
     '''publish a status on Twitter'''
-    
-    config_file=os.path.join(os.environ['HOME'],'.lastfm2tweet')
+
+    config_file = os.path.join(os.environ['HOME'], '.lastfm2tweet')
     try:
         config = open(config_file)
     except IOError, e:
         raise Exception('%s' % e)
-            
+
     json_data = config.read()
-    
+
     oauth_param = json.loads(json_data)
-    
-    for param in ('consumer_key','consumer_secret','key','secret'):
+
+    for param in ('consumer_key', 'consumer_secret', 'key', 'secret'):
         if param not in oauth_param:
             raise Exception('missing param %s in config file' % param)
-        
-        #workaround of fucking HMAC function
+
+        # workaround of fucking HMAC function
         oauth_param[param] = oauth_param[param].encode('ascii')
-    
+
     auth = tweepy.OAuthHandler(oauth_param['consumer_key'], oauth_param['consumer_secret'])
     auth.set_access_token(oauth_param['key'], oauth_param['secret'])
     print "Authenticated as %s" % auth.get_username()
@@ -127,8 +131,7 @@ def publish_twitter(text):
     api.update_status(text)
 
 
-
-def main(argv=None): # IGNORE:C0111
+def main(argv=None):  # IGNORE:C0111
     '''Command line options.'''
 
     if argv is None:
@@ -158,16 +161,18 @@ USAGE
     try:
         # Setup argument parser
         parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-v", "--verbose", dest="verbose", action="count", help="set verbosity level [default: %(default)s]")
+        parser.add_argument("-v", "--verbose", dest="verbose", action="count",
+                            help="set verbosity level [default: %(default)s]")
         parser.add_argument("-u", "--username", dest="username", action="store",
                             required=True, help="LastFM username [default: %(default)s]")
         parser.add_argument("-p", "--period", dest="period", action="store",
-                            default='3month',help="LastFM period [default: %(default)s]")
-        parser.add_argument("-c", "--chart", dest="chart", choices=['album','artist','track'],
-                            default ='artist', help="Chart type [default: %(default)s]")
-        
-        parser.add_argument("-t", "--tweet", dest="tweet", action="store_true", help="Publish to Twitter [default: %(default)s]")
-                
+                            default='3month', help="LastFM period [default: %(default)s]")
+        parser.add_argument("-c", "--chart", dest="chart", choices=['album', 'artist', 'track'],
+                            default='artist', help="Chart type [default: %(default)s]")
+
+        parser.add_argument("-t", "--tweet", dest="tweet", action="store_true",
+                            help="Publish to Twitter [default: %(default)s]")
+
         # Process arguments
         args = parser.parse_args()
 
@@ -176,7 +181,6 @@ USAGE
         chart = args.chart
         verbose = args.verbose
         tweet = args.tweet
-        
 
         if verbose > 0:
             print("Verbose mode on")
@@ -185,16 +189,17 @@ USAGE
             else:
                 print("Tweet mode off")
 
-        month=date.today().strftime('%b')
-        text = summarize(get_print_list(username,chart,period), prefix="Top %ss for %s from last.fm:\n"% (chart,month))
-        
+        month = date.today().strftime('%b')
+        text = summarize(get_print_list(username, chart, period),
+                         prefix="Top %ss for %s from last.fm:\n" % (chart, month))
+
         if tweet:
             publish_twitter(text)
         else:
             print text
-            
+
         if verbose > 0:
-                print("Done")
+            print("Done")
 
 
     except KeyboardInterrupt:
@@ -202,25 +207,25 @@ USAGE
         return 0
     except Exception, e:
         if DEBUG or TESTRUN:
-            raise(e)
+            raise (e)
         indent = len(program_name) * " "
         sys.stderr.write(program_name + ": " + repr(e) + "\n")
         sys.stderr.write(indent + "  for help use --help\n")
         return 2
-    
-    
-    
-    
+
+
 if __name__ == "__main__":
     if DEBUG:
         sys.argv.append("-h")
         sys.argv.append("-v")
     if TESTRUN:
         import doctest
+
         doctest.testmod()
     if PROFILE:
         import cProfile
         import pstats
+
         profile_filename = 'lastfmtweet_profile.txt'
         cProfile.run('main()', profile_filename)
         statsfile = open("profile_stats.txt", "wb")
